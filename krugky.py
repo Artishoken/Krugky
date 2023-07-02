09d861146bcc4f99723e06f8f38eb0788c3301bb
Azazell <elrike850@gmail.com>
2023-07-02 20:18:17 +0300

    можем проверить заряд

--- krugky.py
+++ krugky.py
@@ -5,4 +5,9 @@ class Cup:
         self.charge = charge
         self.speed = speed
         self.carring_ability = carring_ability
+    def check_charge(self):
+        return (print ("полетели")if self.charge == 100 else print ("никуда не полетим"))
+        
+cup1 = Cup("картон","на земле",10,0,10)
+cup1.check_charge()
         
