9f78c6fcb7fc73ca9a37865f6ec1ad2760f97780
Azazell <elrike850@gmail.com>
2023-07-02 20:04:14 +0300

    создал класс и  конструктор класса

---
+++ krugky.py
@@ -0,0 +1,8 @@
+class Cup:
+    def __init__(self, material, condition, charge, speed, carring_ability):
+        self.material = material
+        self.condition = condition
+        self.charge = charge
+        self.speed = speed
+        self.carring_ability = carring_ability
+        
