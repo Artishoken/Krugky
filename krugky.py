0169c51b30028df08ddebd6b17214b3fc0474f68
BackDad <manuk_1122@mail.ru>
2023-07-03 10:25:51 +0300

    добавлены аттрибуты координат,дополнен конструткор, создан метод check_line для сверки координат

--- krugky.py
+++ krugky.py
@@ -1,5 +1,5 @@
 class Cup:
-    def __init__(self, material, condition, charge, speed, speed_limit, carring_ability, containing):
+    def __init__(self, material, condition, charge, speed, speed_limit, carring_ability, containing, x, y, z):
         self.material = material
         self.condition = condition
         self.charge = charge
@@ -7,6 +7,9 @@ class Cup:
         self.speed_limit = speed_limit
         self.carring_ability = carring_ability
         self.containing = containing
+        self.x = x
+        self.y = y
+        self.z = z
 
     def drive(self, speed):
         self.charge -= 10
@@ -28,15 +31,60 @@ class Cup:
             print("долейте ещё", self.carring_ability - self.containing)
 
     def check_speed(self):
-        if self.speed == 0:
+        if self.speed != 0:
             print("стоим на месте")
         elif self.speed < self.speed_limit:
             print("летим")
         else:
             print("падаем")
 
+    def check_line(self):
 
-cup1 = Cup("картон", "на земле", 100, 0, 20, 100, 100)
+        # Имитация движения и сравнение координат
+        for _ in range(10):
+            # Имитация движения (пример: изменение координат на случайную величину)
+            cup1.x += 1
+            cup1.y += 1
+            cup1.z += 1
+
+            cup2.x += 2
+            cup2.y += 2
+            cup2.z += 2
+
+            # Сравнение координат и проверка столкновения
+            if (
+                    abs(cup1.x - cup2.x) == 0 and
+                    abs(cup1.y - cup2.y) == 0 and
+                    abs(cup1.z - cup2.z) == 0
+            ):
+                print("Столкновение произошло!")
+                self.speed = 21
+                self.check_speed()
+                break
+
+            # Дополнительные действия при отсутствии столкновения
+            else:
+                # Вывод текущих координат кружек
+                print("Координаты cup1:", cup1.x, cup1.y, cup1.z)
+                print("Координаты cup2:", cup2.x, cup2.y, cup2.z)
+
+
+cup1 = Cup(material="картон",
+           condition="на земле",
+           charge=100,
+           speed=10,
+           speed_limit=20,
+           carring_ability=100,
+           containing=100,
+           x=5, y=5, z=5)
+cup2 = Cup(material="картон",
+           condition="на земле",
+           charge=100,
+           speed=10,
+           speed_limit=20,
+           carring_ability=100,
+           containing=100,
+           x=0, y=0, z=0)
 
 cup1.check_charge()
 
@@ -44,8 +92,11 @@ cup1.power_the_cup(100)
 print(cup1.charge)
 
 cup1.fill_cup()
-
+distance = 100
 cup1.check_speed()
-while cup1.charge > 10:
+while cup1.charge > 10 or distance > 100:
+    distance -= 10
     cup1.drive(100)
-    print(cup1.charge)
+    print(cup1.charge, distance)
+
+cup1.check_line()
